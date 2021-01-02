b6bccd9875590a79abf92718f33412a79fd89673
Jason Axelson <jason.axelson@gmail.com>
2021-01-02 11:39:12 -1000

    Remove mdns configuration

--- fw/config/target.exs
+++ fw/config/target.exs
@@ -81,37 +81,37 @@ config :vintage_net,
     {"wlan0", %{type: VintageNetWiFi}}
   ]
 
-config :mdns_lite,
-  # The `host` key specifies what hostnames mdns_lite advertises.  `:hostname`
-  # advertises the device's hostname.local. For the official Nerves systems, this
-  # is "nerves-<4 digit serial#>.local".  mdns_lite also advertises
-  # "nerves.local" for convenience. If more than one Nerves device is on the
-  # network, delete "nerves" from the list.
-
-  host: [:hostname, "nerves"],
-  ttl: 120,
-
-  # Advertise the following services over mDNS.
-  services: [
-    %{
-      name: "SSH Remote Login Protocol",
-      protocol: "ssh",
-      transport: "tcp",
-      port: 22
-    },
-    %{
-      name: "Secure File Transfer Protocol over SSH",
-      protocol: "sftp-ssh",
-      transport: "tcp",
-      port: 22
-    },
-    %{
-      name: "Erlang Port Mapper Daemon",
-      protocol: "epmd",
-      transport: "tcp",
-      port: 4369
-    }
-  ]
+#config :mdns_lite,
+#  # The `host` key specifies what hostnames mdns_lite advertises.  `:hostname`
+#  # advertises the device's hostname.local. For the official Nerves systems, this
+#  # is "nerves-<4 digit serial#>.local".  mdns_lite also advertises
+#  # "nerves.local" for convenience. If more than one Nerves device is on the
+#  # network, delete "nerves" from the list.
+#
+#  host: [:hostname, "nerves"],
+#  ttl: 120,
+#
+#  # Advertise the following services over mDNS.
+#  services: [
+#    %{
+#      name: "SSH Remote Login Protocol",
+#      protocol: "ssh",
+#      transport: "tcp",
+#      port: 22
+#    },
+#    %{
+#      name: "Secure File Transfer Protocol over SSH",
+#      protocol: "sftp-ssh",
+#      transport: "tcp",
+#      port: 22
+#    },
+#    %{
+#      name: "Erlang Port Mapper Daemon",
+#      protocol: "epmd",
+#      transport: "tcp",
+#      port: 4369
+#    }
+#  ]
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
