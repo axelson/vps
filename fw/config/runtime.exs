48903ca860ac4663c87cdda070ffde1248b94e36
Jason Axelson <jason.axelson@gmail.com>
2021-06-22 12:34:57 -1000

    Upgrade to the latest nerves system
    Having a `config/runtime.exs` file apparently is treated as a
    Config.Provider which nerves doesn't fully support at the moment

--- fw/config/runtime.exs
+++
@@ -1,5 +0,0 @@
-config :gviz, GVizWeb.Endpoint,
-  secret_key_base: "ceI+YsDLAW7ZysZivz9xpu9d+eLo/5/E9TS2bPKPmZz1PrQ7H6iF5jm2CHMU71j/"
-
-config :gviz, MakeupLiveWeb.Endpoint,
-  secret_key_base: "7uIEeJiHEIc8ikQb07KqrPu3cTpCiodskjUL+TdUFnfERXaGLsX4XuCjLwASbVWZ"
